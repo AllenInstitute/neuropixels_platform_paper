d74ba2eb57736b1c3c8dadf4cb22dedb4e5b23d4
Josh Siegle <joshs@alleninstitute.org>
2019-10-15 13:15:54 -0700

    Add code for Figure1

---
+++ Figure1/Figure1_example_raster.py
@@ -0,0 +1,134 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
+from allensdk.brain_observatory.ecephys.ecephys_session import EcephysSession 
+from allensdk.brain_observatory.ecephys.stimulus_analysis import ReceptiveFieldMapping
+from allensdk.brain_observatory.ecephys.stimulus_analysis import DriftingGratings
+
+
+cache_directory = "/mnt/hdd0/cache_dir_10_08"
+
+manifest_path = os.path.join(cache_directory, "manifest.json")
+
+cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
+
+# %%
+
+sessions = cache.get_sessions()
+session_id = 756029989
+session = cache.get_session_data(session_id)
+dg = DriftingGratings(session)
+probe_id = session.probes.index.values[3]
+lfp = session.get_lfp(probe_id)
+
+# %%
+
+plt.figure(1711)
+plt.clf()
+    
+trial_number = 248
+
+areas = ('VISp', 'VISl', 'VISrl', 'VISal', 'VISpm', 'VISam', 'LGd', 'LP')
+
+total_units = 0
+
+freq1 = dg.stim_table.iloc[trial_number-1].temporal_frequency
+freq2 = dg.stim_table.iloc[trial_number].temporal_frequency
+freq3 = dg.stim_table.iloc[trial_number+1].temporal_frequency
+
+t1 = np.linspace(0,2*pi*freq1*2,200)
+t2 = np.linspace(0,2*pi*freq2*2,200)
+t3 = np.linspace(0,2*pi*freq3*2,200)
+z = np.zeros((100,))
+
+a = np.concatenate((z,t1,z,t2,z,t3,z),axis=0)
+
+num_plots = 13
+
+plt.subplot(num_plots,1,1)
+plt.plot(1-np.sin(a))
+plt.xlim([0,len(a)])
+plt.ylim([-1,3])
+plt.axis('off')
+
+for area_idx, area in enumerate(areas):
+    
+    units = session.units[(session.units.manual_structure_acronym == area)].index.values
+                         
+    total_units += len(units)
+
+    t = np.arange(-4,6.0,0.001) 
+    
+    spike_data = session.presentationwise_spike_counts(
+                bin_edges = t,
+                stimulus_presentation_ids = dg.stim_table.index.values[trial_number],
+                unit_ids = units
+            )
+    
+    a = spike_data.sum(dim='time_relative_to_stimulus_onset')
+
+    x, y = np.where(np.squeeze(spike_data.data))
+    
+    plt.subplot(num_plots,1,area_idx+2)
+    
+    plt.scatter(t[x],y,s=1, c='k',marker='|',alpha=0.4)
+    
+    plt.xlim([np.min(t),np.max(t)])
+   
+    plt.axis('off')
+plt.subplot(num_plots,1,1)
+plt.title(total_units)
+
+plt.subplot(num_plots,1,11)
+
+bounds = dg.stim_table.start_time.values[trial_number] + t
+
+selection = (session.running_speed.start_time >= bounds[0]) & \
+                  (session.running_speed.start_time < bounds[-1])
+
+running_speeds = session.running_speed[selection].velocity
+starts = session.running_speed[selection].start_time
+ends = session.running_speed[selection].end_time
+
+t = np.linspace(-4,6,len(running_speeds))
+
+plt.plot(t, running_speeds, 'c')
+plt.plot([t[0],t[-1]],[0,0],'--g')
+plt.ylim([-3,75])
+plt.xlim([-4,6])
+
+plt.subplot(num_plots,1,12)
+
+eye_info = session.get_eye_tracking_data()
+
+selection = (eye_info.index.values >= bounds[0]) & \
+            (eye_info.index.values < bounds[-1])
+
+pupil_x = eye_info.pupil_center_x[selection]
+pupil_y = eye_info.pupil_center_y[selection]
+pupil_width = eye_info.pupil_width[selection]
+
+t = np.linspace(-4,6,len(pupil_width))
+
+
+plt.subplot(num_plots,1,12)
+plt.plot(t, pupil_width, 'm')
+plt.xlim([-4,6])
+
+lfp_plot = lfp.loc[dict(time=slice(np.min(bounds),np.max(bounds)))]
+
+D = lfp_plot.data[:,40] * 1e6
+plt.subplot(num_plots,1,10)
+
+t = np.linspace(-4,6,len(D))
+
+plt.plot(t,D,'-r',linewidth=0.8)
+
+plt.xlim([np.min(t), np.max(t)])
+
+plt.ylim([-1500,1500])
+
+
---
+++ Figure1/Figure1_unit_yield.py
@@ -0,0 +1,129 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# note: use 'get_data_from_warehouse' to generate the 'df' object
+
+areas = ('VISp','VISl','VISrl','VISal','VISpm','VISam','LGd','LP') 
+num_units_per_area = np.zeros((len(df.specimen_id.unique()), len(areas)))
+num_overlap_per_area = np.zeros((len(df.specimen_id.unique()), len(areas), len(areas)))
+
+min_units = 0
+
+num_per_mouse = []
+
+for mouse_idx, mouse in enumerate(df.specimen_id.unique()):
+    
+    selection1 =  (df.specimen_id == mouse) & \
+                  (df.amplitude_cutoff < 0.1) & \
+                  (df.presence_ratio > 0.95) & \
+                  (df.isi_violations < 0.5) & \
+                  (df.quality == 'good')
+                  
+    num_per_mouse.append(np.sum(selection1))
+    
+    for area_idx, area in enumerate(areas):
+        
+        selection2 = (df.structure_acronym == area)
+        
+        if np.sum(selection1 * selection2) > min_units:
+                    
+            num_units_per_area[mouse_idx, area_idx] = np.sum(selection1 & selection2)
+        
+simultaneous_areas = np.sum(num_units_per_area[:,:].astype('bool'),1)
+did_record = np.sum(num_units_per_area.astype('bool'),0)
+
+# %%
+
+num_per_probe = []
+has_ccf = 0
+
+for probe_idx, probe in enumerate(df.ecephys_probe_id.unique()):
+    
+    selection = (df.ecephys_probe_id == probe) & \
+                    (df.amplitude_cutoff < 0.1) & \
+                    (df.presence_ratio > 0.95) & \
+                    (df.isi_violations < 0.5)
+    
+    num_per_probe.append(np.sum(selection))
+    
+    if np.sum(df[selection]['anterior_posterior_ccf_coordinate']) > 0:
+        
+        has_ccf += 1
+
+print('Total units per probe:')
+print('Mean: ' + str(np.mean(num_per_probe)) + '+/-' + str(np.std(num_per_probe)))
+print()
+
+print('Total units per mouse:')
+print('Mean: ' + str(np.mean(num_per_mouse)) + '+/-' + str(np.std(num_per_mouse)))
+print()
+
+# %%
+
+unit_count = num_units_per_area.flatten()
+unit_count[unit_count == 0] = np.nan
+unit_mean = np.nanmean(unit_count)
+unit_std = np.nanstd(unit_count)
+
+print('Total units per area:')
+print('Mean: ' + str(unit_mean) + '+/-' + str(unit_std))
+print()
+print('Total areas per recording:')
+print('Mean: ' + str(np.mean(simultaneous_areas)) + '+/-' + str(np.std(simultaneous_areas)))
+
+# %%
+plt.figure(4171)
+plt.clf()
+
+plt.subplot2grid((1,6),(0,4),colspan=2,rowspan=1)
+
+common_area_names = ['V1','LM','RL','AL','PM','AM','LGd','LP'] #,'CA1', 'CA3', 'DG', 'SUB']
+
+h,b = np.histogram(simultaneous_areas, bins=np.arange(2,11)) #, color='gray')
+plt.bar(b[:-1],h,width=1.0,color='gray', alpha=0.5)
+plt.xlabel('Number of simultaneous visual areas')
+plt.ylabel('Number of experiments')
+plt.ylim([0,22])
+plt.yticks(ticks=np.arange(0,22,5))
+
+ax = plt.gca()
+[ax.spines[loc].set_visible(False) for loc in ['right', 'top']]   
+
+ds = []
+
+for i in range(len(common_area_names)):
+    N = num_units_per_area[:,i]
+    N = N[N > 0]
+    ds.append(N)
+    
+plt.subplot2grid((1,6),(0,0),colspan=4,rowspan=1)
+
+ax = plt.gca()
+
+box_parts = plt.boxplot(ds, patch_artist=True)
+plt.xticks(ticks=np.arange(len(common_area_names))+1, labels=common_area_names, fontsize=12)
+plt.ylabel('Number of units per experiment')
+
+for i in range(8):
+    
+    plt.text(i+0.85,box_parts['caps'][i*2+1].get_data()[1][0]+5,str(did_record[i]),fontsize=12)
+
+for i, pc in enumerate(box_parts['boxes']):
+    pc.set_facecolor(get_color_palette(areas[i],'seaborn'))
+    pc.set_edgecolor('k')
+    pc.set_alpha(0.9)
+    
+for i, pc in enumerate(box_parts['medians']):
+    pc.set_color('k')
+
+plt.xlim([-0.5,9.5])
+plt.ylim([0,180])
+
+ax = plt.gca()
+[ax.spines[loc].set_visible(False) for loc in ['right', 'top']]   
+
+plt.tight_layout()
+
+# %%
+
+
